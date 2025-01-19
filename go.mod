4908171ce481e8499a3ad5e2a6ee7aa346e22335
edieguez <dieguezemmanuel@gmail.com>
2025-01-19 10:00:48 -0600

    Add big barks methods

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/edieguez/dog
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package dog
+
+import "strings"
+
+func WhenGrownUp(bark string) string {
+	return "When grown up, the dog says " + strings.ToUpper(bark)
+}
